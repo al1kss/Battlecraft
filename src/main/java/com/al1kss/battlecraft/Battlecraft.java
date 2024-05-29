b7c2c5a9a666d6e0bedd364ee8e2cd9c4494af0d
al1k <abdykaimovalikrut01@gmail.com>
2024-05-29 18:03:55 +0600

    1.0

---
+++ src/main/java/com/al1kss/battlecraft/Battlecraft.java
@@ -0,0 +1,17 @@
+package com.al1kss.battlecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Battlecraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
