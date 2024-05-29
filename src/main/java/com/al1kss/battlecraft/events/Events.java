10cb46a9608fcb97782cb981e9e63423c8bfeb33
al1k <abdykaimovalikrut01@gmail.com>
2024-05-30 01:33:43 +0600

    1.2-(working-banner)

--- src/main/java/com/al1kss/battlecraft/events/Events.java
+++ src/main/java/com/al1kss/battlecraft/events/Events.java
@@ -1,19 +1,47 @@
 package com.al1kss.battlecraft.events;
 
 import com.al1kss.battlecraft.Battlecraft;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
+import org.bukkit.World;
+import org.bukkit.entity.*;
+import org.bukkit.Location;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 
 public class Events {
     public static void playerGlow(Player sender){
 
+    }
+
+    public static void createStand(Player player, Material banners){
+        World world = player.getWorld();
+        Location location = player.getLocation();
+
+        ArmorStand armorStand = world.spawn(location, ArmorStand.class);
+        armorStand.setVisible(false);
+        armorStand.setBasePlate(false);
+        armorStand.setCollidable(false);
+        armorStand.setInvulnerable(true);
+        armorStand.setArms(true);
+
+
+        ItemStack banner = new ItemStack(banners);
+        armorStand.getEquipment().setItemInMainHand(banner);
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (!player.isOnline() || armorStand.isDead() || !player.getInventory().contains(banner)) {
+                    this.cancel();
+                    armorStand.remove();
+                    return;
+                }
+
+                Location playerLocation = player.getLocation();
+                armorStand.teleport(playerLocation.add(0, 1.0, 0)); // Adjust the Y coordinate as needed
+            }
+        }.runTaskTimer(JavaPlugin.getProvidingPlugin(Battlecraft.class), 0L, 1L);
     }
 }
--- src/main/java/com/al1kss/battlecraft/listeners/Listeners.java
+++ src/main/java/com/al1kss/battlecraft/listeners/Listeners.java
@@ -1,18 +1,16 @@
 package com.al1kss.battlecraft.listeners;
 
 import com.al1kss.battlecraft.Battlecraft;
-import org.bukkit.Bukkit;
-import org.bukkit.Color;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.entity.Chicken;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
+import com.al1kss.battlecraft.events.Events;
+import org.bukkit.*;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -20,29 +18,60 @@ import org.bukkit.scheduler.BukkitRunnable;
 import java.util.List;
 import java.util.Objects;
 
+import static com.al1kss.battlecraft.events.Events.createStand;
+
 
 public class Listeners implements Listener {
     private Plugin Listeners;
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-
-
         final Player player = event.getPlayer();
+
+        invCheck(player.getInventory());
 
     }
 
     @EventHandler
     public void onDroppedItem(PlayerDropItemEvent event) {
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                for (Entity entity : Objects.requireNonNull(Bukkit.getWorld("world")).getEntities()) {
-                    if (entity instanceof Item && entity.getCustomName() != null && entity.getCustomName().equals("Blue Banner")) {
-                        entity.setGlowing(true);
-                    }
+
+    }
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        invCheck(player.getInventory());
+    }
+
+
+    public void invCheck(PlayerInventory inventory){
+        Player player = (Player) inventory.getHolder();
+        World world = player.getWorld();
+        Material red = Material.RED_BANNER;
+        Material blue = Material.BLUE_BANNER;
+        boolean hasRedBannerStand = false;
+        boolean hasBlueBannerStand = false;
+
+        for (Entity entity : world.getEntities()) {
+            if (entity instanceof ArmorStand) {
+                ArmorStand armorStand = (ArmorStand) entity;
+                ItemStack itemInHand = armorStand.getEquipment().getItemInMainHand();
+
+                if (itemInHand != null && itemInHand.getType() == red) {
+                    hasRedBannerStand = true;
+                } else if (itemInHand != null && itemInHand.getType() == blue) {
+                    hasBlueBannerStand = true;
+                }
+
+                if (hasRedBannerStand && hasBlueBannerStand) {
+                    break;
                 }
             }
-        }.runTaskTimer(Battlecraft.instance, 0L, 20L);
+        }
+
+        if (inventory.contains(red) && !hasRedBannerStand) {
+            createStand(player, red);
+        } else if (inventory.contains(blue) && !hasBlueBannerStand) {
+            createStand(player, blue);
+        }
     }
 }
